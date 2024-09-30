6f1156c7fb51cc8713331249dde626d6f6168efe
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-10-01 01:13:10 +0530

    Setup the Project and Initial Project Structure

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import 'app_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyAppView();
+  }
+}
---
+++ lib/app_view.dart
@@ -0,0 +1,26 @@
+import 'package:expense_wise/screens/home/views/home_screens.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:"Expense Tracker",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground:Colors.black,
+          primary:Color(0xFF00B2E7) ,
+          secondary:Color(0XFFE064F7),
+          tertiary: Color(0XFFFF8D6C)
+
+        )
+      ),
+home:HomeScreen(),
+    );
+  }
+}
