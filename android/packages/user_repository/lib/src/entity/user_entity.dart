0d3df3fe39a35ed86f4fd6f32efa08cfc215fc43
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-10-13 00:09:50 +0530

    feat:define entities and modals for user Authentications

---
+++ android/packages/user_repository/lib/src/entity/user_entity.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable{
+
+  final String userId;
+  final String email;
+  final String name;
+
+  const MyUserEntity({
+    required this.userId,
+    required this.email,
+    required this.name
+
+  });
+
+
+  Map<String,Object?>toDocument(){
+    return{
+      'userId':userId,
+      'email':email,
+      'name':name,
+    };
+  }
+  @override
+  List<Object> get props=>[userId,email,name];
+}
